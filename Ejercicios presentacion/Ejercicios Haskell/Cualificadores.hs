e097e09356d76ca88ca2f520e4ab9c938391b4ed
Alvaro <aazarabandag@correo.udistrital.edu.co>
2019-10-04 11:52:29 -0500

    se añade gitignore y se ponen todos los ejercicios de funciones en un solo modulo

--- Ejercicios presentacion/Ejercicios Haskell/Cualificadores.hs
+++
@@ -1,3 +0,0 @@
-cualif1 = print([x | x <- [1 .. 12]])
-cualif2 = print([x+x | x <- [1 .. 12]])
-cualif3 = print([x*x | x <- [1..10]])
--- Ejercicios presentacion/Ejercicios Python/Listas_python.py
+++
@@ -1,8 +0,0 @@
-lista1 = [1,2,3,6,4];
-print (len(lista1));
-
-lista2 = [1,2,3,4,5,6,7,2,123,88];
-
-lista1lista2 = lista1 + lista2;
-print(lista1lista2);
-
