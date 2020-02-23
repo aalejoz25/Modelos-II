fb58b1fb0d0651b9e635505949f321e6e4643173
Alvaro Zarabanda <aazarabandag@correo.udistrital.edu.co>
2020-02-23 18:56:23 -0500

    se añaden primeros ejercicios en python

---
+++ Ejercicios Python/Ejercicios simples/Ejercicio 3.py
@@ -0,0 +1,8 @@
+"""Escribir un programa que sume, reste, multiplique
+y divida dos números leídos desde el teclado."""
+
+x = int(input("Digite el primer numero entero: "))
+y = int(input("Digite el segundo numero entero: "))
+
+print("La suma de los numeros es: ",x+y)
+
---
+++ Ejercicios Python/Ejercicios simples/Ejercicio 7.py
@@ -0,0 +1,9 @@
+"""Escribir un programa que calcule el volumen de una esfera de radio = r
+volumen de la esfera = 4/3 * PI * radio^3"""
+import math
+
+radio = float(input("Digite el radio de la esfera: "))
+
+volumen = (4/3)*(math.pi)*pow(radio,3)
+
+print("El volumen de la esfera es: ", volumen)
