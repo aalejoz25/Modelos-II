fc1af82e4bf86406394debe127f280dcc5adfadf
Alvaro Zarabanda <aazarabandag@correo.udistrital.edu.co>
2020-02-23 19:13:12 -0500

    se suben ejercicios con condicionales

---
+++ Ejercicios Python/Ejercicios con condicionales/Ejercicio 18.py
@@ -0,0 +1,9 @@
+"""Escribir un programa que determine si un número
+leído desde el teclado es par o impar"""
+
+x = int(input("Digite un numero entero: "))
+
+if (x%2==0):
+    print("El numero es par")
+else:
+    print("el numero es impar")
---
+++ Ejercicios Python/Ejercicios con condicionales/Ejercicio 22.py
@@ -0,0 +1,9 @@
+"""Escribir un programa que lea un caracter e indique si es o no una vocal."""
+
+x = input("Digite un caracter: ")
+
+if (x=="a" or x=="e" or x=="i" or x=="o" or x=="u" or x=="A" or x=="E" or x=="I" or x=="O" or x=="U"):
+    print("El caracter es una vocal")
+else:
+    print("El caracter es consonante")
+    
