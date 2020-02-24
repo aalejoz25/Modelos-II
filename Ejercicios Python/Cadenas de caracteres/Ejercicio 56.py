5270b55fc5d6ea410247bd9f13262b18df7cfb8c
Alvaro Zarabanda <aazarabandag@correo.udistrital.edu.co>
2020-02-24 07:39:58 -0500

    ejercicios con caracteres

---
+++ Ejercicios Python/Cadenas de caracteres/Ejercicio 56.py
@@ -0,0 +1,10 @@
+"""Escribir un programa que elimine los blancos de una cadena de caracteres. La cadena
+original y la transformada deben almacenarse de forma independiente."""
+
+cadenao = input("Digite una cadena de texto: ")
+
+cadenat = cadenao.replace(" ","")
+
+
+print("cadena original:",cadenao)
+print("la cadena sin espacios es:",cadenat)
