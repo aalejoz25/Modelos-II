9da06ca11165ec4c3a0a06cf257dc6c162f2ab45
Alvaro Zarabanda <aazarabandag@correo.udistrital.edu.co>
2020-02-24 10:37:21 -0500

    ejercicio modulos

--- Ejercicios Python/Modulos/Ejercicio 81/Operaciones.py
+++ Ejercicios Python/Modulos/Ejercicio 81/Operaciones.py
@@ -1,19 +1,20 @@
-
+resultado = 0
 
 def suma (x,y):
     print(x+y)
 
 def resta (x,y):
-    print(x+y)
+    print(x-y)
 
 def multiplicacion (x,y):
-    print(x+y)
+    print(x*y)
 
 def division (x,y):
-    print(x+y)
+    print(x/y)
 
-def potencia (x, y):
-    if (exponente == 0):
-        print(1)
+def potencia (x,y):
+    if (y == 0):
+        return 1
     else:
-        print(base*potencia(base,exponente-1))
+        resultado = x*potencia(x,y-1)
+        return resultado
--- Ejercicios Python/Modulos/Ejercicio 81/main.py
+++ Ejercicios Python/Modulos/Ejercicio 81/main.py
@@ -1,5 +1,12 @@
 import Operaciones
 
-
 x = int(input("Digite el primer numero: "))
 y = int(input("Digite el segundo numero: "))
+
+Operaciones.suma(x,y)
+Operaciones.resta(x,y)
+Operaciones.multiplicacion(x,y)
+Operaciones.division(x,y)
+potencia = Operaciones.potencia(x,y)
+
+print(potencia)
