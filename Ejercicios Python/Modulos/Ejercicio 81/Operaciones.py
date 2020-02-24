3cb79e0e11d2a31468e090b5be94b372acc02c69
Alvaro Zarabanda <aazarabandag@correo.udistrital.edu.co>
2020-02-24 08:19:19 -0500

    ejercicio modulos

---
+++ Ejercicios Python/Modulos/Ejercicio 81/Operaciones.py
@@ -0,0 +1,19 @@
+
+
+def suma (x,y):
+    print(x+y)
+
+def resta (x,y):
+    print(x+y)
+
+def multiplicacion (x,y):
+    print(x+y)
+
+def division (x,y):
+    print(x+y)
+
+def potencia (x, y):
+    if (exponente == 0):
+        print(1)
+    else:
+        print(base*potencia(base,exponente-1))
---
+++ Ejercicios Python/Modulos/Ejercicio 81/main.py
@@ -0,0 +1,7 @@
+import Operaciones
+
+
+x = int(input("Digite el primer numero: "))
+y = int(input("Digite el segundo numero: "))
+
+
