9d9ef5e9b96c41d7933c88ac466362adc8412600
Alvaro Zarabanda <aazarabandag@correo.udistrital.edu.co>
2020-02-26 21:40:26 -0500

    ej 90

---
+++ Ejercicios Python/Ejercicios varios/Ejercicio 90.py
@@ -0,0 +1,28 @@
+import time
+def convertir(h,m,s):
+       s=s/3600
+       m=m/60
+       h=h
+       suma=s+m+h
+       return suma
+def contar(h,m,s):
+       horas=0
+       segun=0
+       minu=0
+       x='q'
+       while (x !='ok'):
+           segun=segun+1           
+           if(segun==60):
+               segun=0
+               minu=minu+1
+           if(minu==60):    
+               minu=0
+               horas=horas+1
+           print(str(horas)+':'+str(minu)+':'+str(segun))
+           if(horas==h and minu==m and segun==s):
+               x='ok'
+           time.sleep(1)
+horas=input('ingrese hora: ')
+minutos=input('ingrese minutos: ')
+segundos=input('ingrese segundos: ')
+limite=contar(int(horas),int(minutos),int(segundos))
