79bbb45eda5d2b1dc3a42ddddc09caf8105a669f
Alvaro Zarabanda <aazarabandag@correo.udistrital.edu.co>
2020-03-18 12:54:09 -0500

    HV 2

---
+++ Ejercicios HTML/Hoja de vida HTML V2/js/functions.js
@@ -0,0 +1,15 @@
+function guardarDatos() {
+    localStorage.nombre = document.getElementById("nombre").value;
+    localStorage.celular = document.getElementById("cel").value;
+    localStorage.email = document.getElementById("mail").value;
+    localStorage.edad = document.getElementById("age").value;
+
+    localStorage.profile = document.getElementById("profile").value;
+
+    localStorage.celular = document.getElementById("cel").value;
+
+
+
+
+
+}
